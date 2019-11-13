1ac7c61b7323df025e8abffe89bbe939c3e5f9c3
Emily <emily_ring@ymail.com>
2019-11-13 11:45:29 -0500

    Started new JS forms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,16 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import InfoForm from '../infoForm.vue'
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue'
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  if (document.querySelector('#infoForm')) {
+    new Vue({ render: h => h(InfoForm) }).$mount('#infoForm')
+  }
+})
